d56abaed668af490a07d1e0a964a8d9dafad797b
davelasquez12 <davelasquez12@gmail.com>
2022-05-13 11:32:08 -0500

    Update login user function to be a POST

--- netlify/functions/users/main.go
+++ netlify/functions/users/main.go
@@ -2,32 +2,34 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/UVie-Clash2022/uvie-backend/models"
 	"github.com/UVie-Clash2022/uvie-backend/server"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"strings"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+
 	if request.HTTPMethod == "POST" {
+		subPaths := strings.Split(request.Path, "/")
+		method := subPaths[len(subPaths)-1]
+		fmt.Println("method: " + method)
+
 		var user models.User
 		err := json.Unmarshal([]byte(request.Body), &user)
 		if err != nil {
 			return server.Get500ServerError(err)
 		}
 
-		return SignUpUser(user.Username, user.Password)
-	}
-
-	if request.HTTPMethod == "GET" {
-		username := request.QueryStringParameters["username"]
-		password := request.QueryStringParameters["password"]
-
-		if username == "" || password == "" {
-			return server.Get400ServerError("Invalid username or password")
+		if method == "login" {
+			return LoginUser(user.Username, user.Password)
+		} else if method == "signup" {
+			return SignUpUser(user.Username, user.Password)
+		} else {
+			return server.Get400ServerError("Cannot process request")
 		}
-
-		return LoginUser(username, password)
 	}
 
 	return &events.APIGatewayProxyResponse{
